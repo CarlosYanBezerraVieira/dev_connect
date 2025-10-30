c903c2b27a8c80960778a3ba3315131a7b7c22c7
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-10-29 23:16:59 -0300

    feat: add new routes

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -3,23 +3,20 @@ import 'package:go_router/go_router.dart';
 import 'package:dev_connect/features/auth/login_page.dart';
 import 'package:dev_connect/features/feed/feed_page.dart';
 import 'package:dev_connect/features/post/post_detail_page.dart';
+import 'package:dev_connect/features/post/upsert_post_page.dart';
 
 class AppRoutes {
-  // telas de autenticação / fluxo público
   static const String login = '/login';
-
-  // tela principal com feed
   static const String feed = '/feed';
-
-  // rota com parâmetro (use seu sistema de roteamento para substituir :id)
   static const String postDetail = '/post/:id';
+  static const String postUpsert = '/post/create';
 }
 
-/// Nomes das rotas usados pelo GoRouter (valores curtos, reutilizáveis).
 class RouteNames {
   static const String login = 'login';
   static const String feed = 'feed';
   static const String postDetail = 'postDetail';
+  static const String postCreate = 'postCreate';
 }
 
 final GoRouter appRouter = GoRouter(
@@ -43,13 +40,13 @@ final GoRouter appRouter = GoRouter(
         return PostDetailPage(postId: postId);
       },
     ),
+    GoRoute(
+      path: AppRoutes.postUpsert,
+      name: RouteNames.postCreate,
+      builder: (BuildContext context, GoRouterState state) {
+        final String? id = state.extra is String ? state.extra as String : null;
+        return UpsertPostPage(postId: id);
+      },
+    ),
   ],
-  // redirect: (context, state) {
-  //   const bool isLoggedIn = false;
-  //   final loggingIn = state.matchedLocation == AppRoutes.login;
-
-  //   if (!isLoggedIn && !loggingIn) return AppRoutes.login;
-  //   if (isLoggedIn && loggingIn) return AppRoutes.feed;
-  //   return null;
-  // },
 );
